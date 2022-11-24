9aa705d73c2bb0f386ebffd9351465ec2abe5dc0
Blastus <noctis.skytower+github@gmail.com>
2022-11-23 19:57:20 -0600

    Modifies the safe_tkinter module so that its code is up to same standard as the rest of the other modules.

--- safe_tkinter.py
+++ safe_tkinter.py
@@ -1,43 +1,83 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Register tkinter classes with threadbox for immediate usage.
 
 This module clones several classes from the tkinter library for use with
 threads. Instances from these new classes should run on whatever thread
 the root was created on. Child classes inherit the parent's safety."""
 
-__author__ = 'Stephen "Zero" Chappell <Noctis.Skytower@gmail.com>'
-__date__ = '4 June 2012'
-__version__ = 1, 0, 0
-
-################################################################################
-
+import datetime
 import time
 import tkinter.filedialog
 import tkinter.font
 import tkinter.messagebox
 import tkinter.scrolledtext
 import tkinter.ttk
+
+import _tkinter
+
 import threadbox
 
-################################################################################
+# Public Names
+__all__ = (
+    'mainloop',
+    'Canvas',
+    'Listbox',
+    'Menu',
+    'PhotoImage',
+    'Spinbox',
+    'Text',
+    'Tk',
+    'Toplevel',
+    'OldButton',
+    'OldFrame',
+    'BooleanVar',
+    'DoubleVar',
+    'IntVar',
+    'StringVar',
+    'Button',
+    'Checkbutton',
+    'Entry',
+    'Frame',
+    'Label',
+    'Labelframe',
+    'Progressbar',
+    'Radiobutton',
+    'Scale',
+    'Scrollbar',
+    'Sizegrip',
+    'Treeview',
+    'Directory',
+    'Font',
+    'Message',
+    'Open',
+    'ScrolledText'
+)
+
+# Module Documentation
+__version__ = 2, 0, 0
+__date__ = datetime.date(2022, 11, 23)
+__author__ = 'Stephen Paul Chappell'
+__credits__ = 'CSC-532'
 
 tkinter.NoDefaultRoot()
 
+
 @threadbox.MetaBox.thread
 def mainloop(self):
-    "Creates a synthetic main loop so that threads can still run."
+    """Creates a synthetic main loop so that threads can still run."""
     while True:
         try:
             self.update()
         except tkinter.TclError:
             break
         else:
-            time.sleep(tkinter._tkinter.getbusywaitinterval() / 1000)
+            time.sleep(_tkinter.getbusywaitinterval() / 1000)
+
 
 threadbox.MetaBox.clone(tkinter.Misc, {'mainloop': mainloop})
 
-################################################################################
-
+# Widget Classes From tkinter
 Canvas = threadbox.MetaBox.clone(tkinter.Canvas)
 Listbox = threadbox.MetaBox.clone(tkinter.Listbox)
 Menu = threadbox.MetaBox.clone(tkinter.Menu)
@@ -46,17 +86,16 @@ Spinbox = threadbox.MetaBox.clone(tkinter.Spinbox)
 Text = threadbox.MetaBox.clone(tkinter.Text)
 Tk = threadbox.MetaBox.clone(tkinter.Tk)
 Toplevel = threadbox.MetaBox.clone(tkinter.Toplevel)
-
 OldButton = threadbox.MetaBox.clone(tkinter.Button)
 OldFrame = threadbox.MetaBox.clone(tkinter.Frame)
 
+# Variable Classes From tkinter
 BooleanVar = threadbox.MetaBox.clone(tkinter.BooleanVar)
 DoubleVar = threadbox.MetaBox.clone(tkinter.DoubleVar)
 IntVar = threadbox.MetaBox.clone(tkinter.IntVar)
 StringVar = threadbox.MetaBox.clone(tkinter.StringVar)
 
-################################################################################
-
+# Widget Classes From ttk
 Button = threadbox.MetaBox.clone(tkinter.ttk.Button)
 Checkbutton = threadbox.MetaBox.clone(tkinter.ttk.Checkbutton)
 Entry = threadbox.MetaBox.clone(tkinter.ttk.Entry)
@@ -70,8 +109,7 @@ Scrollbar = threadbox.MetaBox.clone(tkinter.ttk.Scrollbar)
 Sizegrip = threadbox.MetaBox.clone(tkinter.ttk.Sizegrip)
 Treeview = threadbox.MetaBox.clone(tkinter.ttk.Treeview)
 
-################################################################################
-
+# Miscellaneous Classes From tkinter
 Directory = threadbox.MetaBox.clone(tkinter.filedialog.Directory)
 Font = threadbox.MetaBox.clone(tkinter.font.Font)
 Message = threadbox.MetaBox.clone(tkinter.messagebox.Message)
